f558cb8a168ff866d8a8fdc7f8514ba1a8fbb4b3
Doreen <doreenmarie_cee@hotmail.com>
2023-09-01 18:49:27 +0200

    completing the tasks for the class Saxon

--- src/viking.js
+++ src/viking.js
@@ -17,23 +17,44 @@ class Soldier {
 }
 
 // Viking
-class Viking extends Soldier{
+class Viking extends Soldier {
     constructor (name, health, strength) {
         super(health, strength);
         this.name = name;
 
-    } if (this.health > 0) {
-        return `${this.name} has received ${damage} points of damage`;
-    } else {
-        return `${this.name} has died in act of combat`;
+    } 
+
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
     }
 
+    battleCry (){
+        return `Odin Owns You All!`
+    }
 }
         
         
         
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health,strength);
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
 class War {}
